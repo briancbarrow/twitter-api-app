ba8799abb2a2f109e94beb1cc0c340102e07fe69
Brian Barrow <brian.barrow@nicholas.int>
2016-09-13 13:53:39 -0600

    added array of states and woeid's

---
+++ app.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  var woeid = 2347559;
+  var states = [
+    {name: 'Alabama', woeid: 2347559},
+    {name: 'Alaska', woeid: 2347560},
+    {name: 'Arizona', woeid: 2347561},
+    {name: 'Arkansas', woeid: 2347562},
+    {name: 'California', woeid: 2347563},
+    {name: 'Colorado', woeid: 2347564},
+    {name: 'Connecticut', woeid: 2347565},
+    {name: 'Delaware', woeid: 2347566},
+    // {name: 'Washington DC', woeid: 2347567},
+    {name: 'Florida', woeid: 2347568},
+    {name: 'Georgia', woeid: 2347569},
+    {name: 'Hawaii', woeid: 2347570},
+    {name: 'Idaho', woeid: 2347571},
+    {name: 'Illinois', woeid: 2347572},
+    {name: 'Indiana', woeid: 2347573},
+    {name: 'Iowa', woeid: 2347574},
+    {name: 'Kansas', woeid: 2347575},
+    {name: 'Kentucky', woeid: 2347576},
+    {name: 'Louisiana', woeid: 2347577},
+    {name: 'Maine', woeid: 2347578},
+    {name: 'Maryland', woeid: 2347579},
+    {name: 'Massachusetts', woeid: 2347580},
+    {name: 'Michigan', woeid: 2347581},
+    {name: 'Minnesota', woeid: 2347582},
+    {name: 'Mississippi', woeid: 2347583},
+    {name: 'Missouri', woeid: 2347584},
+    {name: 'Montana', woeid: 2347585},
+    {name: 'Nebraska', woeid: 2347586},
+    {name: 'Nevada', woeid: 2347587},
+    {name: 'New Hampshire', woeid: 2347588},
+    {name: 'New Jersey', woeid: 2347589},
+    {name: 'New Mexico', woeid: 2347590},
+    {name: 'New York', woeid: 2347591},
+    {name: 'North Carolina', woeid: 2347592},
+    {name: 'North Dakota', woeid: 2347593},
+    {name: 'Ohio', woeid: 2347594},
+    {name: 'Oklahoma', woeid: 2347595},
+    {name: 'Oregon', woeid: 2347596},
+    {name: 'Pennsylvania', woeid: 2347597},
+    {name: 'Rhode Island', woeid: 2347598},
+    {name: 'South Carolina', woeid: 2347599},
+    {name: 'South Dakota', woeid: 2347600},
+    {name: 'Tennessee', woeid: 2347601},
+    {name: 'Texas', woeid: 2347602},
+    {name: 'Utah', woeid: 2347603},
+    {name: 'Vermont', woeid: 2347604},
+    {name: 'Virginia', woeid: 2347605},
+    {name: 'Washington', woeid: 2347606},
+    {name: 'West Virginia', woeid: 2347607},
+    {name: 'Wisconsin', woeid: 2347608},
+    {name: 'Wyoming', woeid: 2347609}
+  ];
+
+  // for(var i = 0; i < states.length; i++){
+  //   states[i].woeid = woeid + i;
+  //   console.log((i+1) +'. ' + states[i].name + ' ' + states[i].woeid)
+  // }
+
+});
